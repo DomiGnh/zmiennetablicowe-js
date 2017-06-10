3805cd2232bdf5cc33a02971979a1caa607f4917
Dominika Gnich <d.gnich@wp.pl>
2017-06-10 11:20:14 +0200

    init

--- js/tablice.js
+++ js/tablice.js
@@ -9,4 +9,15 @@ imionaStudentow[2] = "Przemek";
 
 console.log(imiona);
 console.log(imionaStudentow);
-consola.log(imionaChlopakow);
+console.log(imionaChlopakow);
+
+
+var wiek = [];
+var wiekStudentow = [20, 25, 28];
+
+console.log(wiek);
+console.log(wiekStudentow);
+
+//var imiona = new Array() to jest to samo co var imiona = [];//
+
+
