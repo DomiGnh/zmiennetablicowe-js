c07eef4bee669692fedd0a47e3dfd8adc7b6b9ce
Dominika Gnich <d.gnich@wp.pl>
2017-06-10 11:13:37 +0200

    init

---
+++ js/tablice.js
@@ -0,0 +1,12 @@
+'use strict';
+//zmienne tablicowe//
+
+var imiona = new Array();
+var imionaStudentow = new Array(9);
+var imionaChlopakow = new Array('Mateusz', 'Michal', 'Krzysiek', 'Sergio');
+
+imionaStudentow[2] = "Przemek";
+
+console.log(imiona);
+console.log(imionaStudentow);
+consola.log(imionaChlopakow);
